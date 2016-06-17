ff6274730fc47c61722c65dde4cfa16dc0027366
Edwin Rivera <edwin.rivera@outlook.com>
2016-06-17 14:23:47 -0400

    added logic to update current train schedule

--- assets/app.js
+++ assets/app.js
@@ -1,20 +1,39 @@
 $( document ).ready(function() {
-	var dataRef = new Firebase("https://trainschedulerhw.firebaseio.com/");
+	var trainData = new Firebase("https://trainschedulerhw.firebaseio.com/");
 		
 		$("#submitBtn").on("click", function(){
 		 	// Get inputs
-		 	name = $('#name').val().trim(); 
-		    dest = $('#dest').val().trim(); 
-		    time = $('#time').val().trim();
-		    freq = $('#freq').val().trim();
+		 	var name = $('#name').val().trim(); 
+		    var dest = $('#dest').val().trim(); 
+		    var time = $('#time').val().trim();
+		    var freq = $('#freq').val().trim();
+
+		    var newTrain = {
+		    	newName: name,
+		    	newDest: dest,
+		    	newTime: time,
+		    	newFreq: freq
+		    }
 		    // Change what is saved in firebase
-		    dataRef.push({
-		    	name: name,
-		        dest: dest, 
-		        time: time, 
-		        freq: freq
-		    });
+		    trainData.push(newTrain);
+
+		    alert("Train data added");
+
+		    $("#name").val("");
+		    $("#dest").val("");
+		    $("#time").val("");
+		    $("#freq").val("");
 				
 			return false;
 	   	});
+
+	   	trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+	   		var name = childSnapshot.val().newName;
+	   		var dest = childSnapshot.val().newDest;
+	   		var time = childSnapshot.val().newTime;
+	   		var freq = childSnapshot.val().newFreq;
+
+	   		$("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + dest + "</td><td>" + time + "</td><td>" + freq + "<td></tr>");
+	   	})
 });
